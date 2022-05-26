47cd6af900846640c6a769284792b7b74791b814
zhoubichuan <zhoubichuan@icloud.com>
2022-05-26 12:44:21 +0800

    🐛 fix(compiler): master

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# 1.发布到master分支
+git add -A
+git commit -m '🐛 fix(compiler): master'
+git remote add gitee https://gitee.com/zhoubichuan/antdpro-express.git
+git pull gitee master
+git push gitee master
+
+
+# 1.发布到master分支
+git add -A
+git commit -m '🐛 fix(compiler): master'
+git remote add github https://github.com/zhoubichuan/antdpro-express.git
+git pull github master
+git push github master
