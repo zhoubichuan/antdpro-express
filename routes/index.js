accbaa5097845667798296547b4a950135f04dd0
zhoubichuan <zhoubichuan@icloud.com>
2022-12-16 00:17:02 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
-module.exports = {
-  userModel: require('./routes/userModel'),
-  noticeModel: require('./routes/noticeModel'),
-  formModel: require('./routes/formModel'),
-  profileModel: require('./routes/profileModel'),
-  ruleModel: require('./routes/ruleModel'),
-  ruleModel1: require('./routes/ruleModel1'),
-  ruleModel2: require('./routes/ruleModel2'),
-  ruleModel3: require('./routes/ruleModel3'),
-  ruleModel4: require('./routes/ruleModel4'),
-  ruleModel5: require('./routes/ruleModel5'),
-  tags: require('./routes/tags'),
+const fs = require('fs')
+const { request } = require('http')
+const files = fs.readdirSync(__dirname)
+let routes = {}
+for (file of files) {
+  if (file !== 'index.js') {
+    routes[file] = require('./' + file)
+  }
 }
+module.exports = routes
