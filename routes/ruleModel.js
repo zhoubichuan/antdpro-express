75b289d923b35394792bca3c242ae422f9716f57
周必川 <zhoubichuan@icloud.com>
2023-08-26 19:33:51 +0800

    update

--- routes/ruleModel.js
+++ routes/ruleModel.js
@@ -11,11 +11,11 @@ router.post("/rule", async (req, res) => {
   let { name = "TradeCode", type = "张三", value = "asdfa" } = req.body;
   let currentId = "1";
   let preRow = await RuleModel.find({}).sort({ updatedAt: -1 }).limit(1);
-  if (!preRow || !preRow.id) {
+  if (!preRow[0] || !preRow[0].id) {
     currentId = "0000001";
   } else {
-    currentId = String(Number(preRow.id) + 1);
-    currentId = "0".repeat(currentId.length - 7) + currentId;
+    currentId = String(Number(preRow[0].id) + 1);
+    currentId = "0".repeat(7 - currentId.length) + currentId;
   }
   let target = await RuleModel.find({ name });
   if (target.length) {
@@ -38,7 +38,10 @@ router.put("/rule", async (req, res) => {
     res.send({ status: "error", message: "没有找到" });
     return;
   }
-  let result = await RuleModel.updateMany({ name ,type , value });
+  let result = await RuleModel.updateMany(
+    { id },
+    { $set: { name, type, value } }
+  );
   return res.json(result);
 });
 // 删除规则
@@ -55,13 +58,7 @@ router.delete("/rule", async (req, res) => {
 //查询
 // router.get('/rule', checkLogin, async (req, res) => {
 router.get("/rule", async (req, res) => {
-  let {
-    current = 1,
-    pageSize = 10,
-    sorter,
-    filter,
-    ...query
-  } = req.query;
+  let { current = 1, pageSize = 10, sorter, filter, ...query } = req.query;
   if (sorter) {
     sorter = sorter ? JSON.parse(sorter) : {};
     sorter[key] = sorter[key] === "ascend" ? 1 : -1;
