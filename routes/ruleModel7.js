5211ef273ed9e92beb1e16bf5fe178cbe2b10f38
周必川 <zhoubichuan@icloud.com>
2023-09-02 21:54:27 +0800

    update

--- routes/ruleModel7.js
+++ routes/ruleModel7.js
@@ -9,12 +9,32 @@ let moment = require("moment");
 // 添加规则
 router.post("/rule7", async (req, res) => {
   let {
-    ip = "",
-    network = "",
-    version = "",
-    city = "",
-    region = "",
-    region_code = "",
+    ip='124.127.74.192',
+    network='124.127.0.0/17',
+    version='IPv4',
+    city='Beijing',
+    region='Beijing',
+    region_code='BJ',
+    country='CN',
+    country_name='China',
+    country_code='CN',
+    country_code_iso3='CHN',
+    country_capital='Beijing',
+    country_tld='.cn',
+    continent_code='AS',
+    in_eu='false',
+    latitude='39.911',
+    longitude='116.395',
+    timezone='Asia/Shanghai',
+    utc_offset='+0800',
+    country_calling_code='+86',
+    currency='CNY',
+    currency_name='Yuan Renminbi',
+    languages='zh-CN,yue,wuu,dta,ug,za',
+    country_area='9596960',
+    country_population='1411778724',
+    asn='AS4847',
+    org='China Networks Inter-Exchange',
   } = req.body;
   let currentId = "1";
   let preRow = await RuleModel7.find({}).sort({ updatedAt: -1 }).limit(1);
@@ -24,11 +44,11 @@ router.post("/rule7", async (req, res) => {
     currentId = String(Number(preRow[0].id) + 1);
     currentId = "0".repeat(7 - currentId.length) + currentId;
   }
-  let target = await RuleModel7.find({ ip });
-  if (target.length) {
-    res.send({ status: "error", message: "规则重复" });
-    return;
-  }
+  // let target = await RuleModel7.find({ ip });
+  // if (target.length) {
+  //   res.send({ status: "error", message: "规则重复" });
+  //   return;
+  // }
   let result = await RuleModel7.create({
     id: currentId,
     ip,
@@ -37,6 +57,26 @@ router.post("/rule7", async (req, res) => {
     city,
     region,
     region_code,
+    country,
+    country_name,
+    country_code,
+    country_code_iso3,
+    country_capital,
+    country_tld,
+    continent_code,
+    in_eu,
+    latitude,
+    longitude,
+    timezone,
+    utc_offset,
+    country_calling_code,
+    currency,
+    currency_name,
+    languages,
+    country_area,
+    country_population,
+    asn,
+    org
   });
   return res.json(result);
 });
